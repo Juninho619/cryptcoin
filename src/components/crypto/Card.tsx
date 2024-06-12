c8a8a5357421a2a468a1ada4eccc470851d2e0b5
Christophe <christophebontemps619@gmail.com>
2024-06-12 15:32:36 +0200

    added search input

--- src/components/crypto/Card.tsx
+++ src/components/crypto/Card.tsx
@@ -19,12 +19,14 @@ function Card() {
   }, []);
   return (
     <div>
+      <input type='search' className='border border-sky-500' />
+      <button className='border border-sky-500'>Search</button>
       {cryptoList &&
         cryptoList.map((crypto) => {
           return (
             <div
               key={crypto.id}
-              className='relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md'>
+              className='relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md justify-center'>
               <div className='relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700'>
                 <img
                   src={crypto.image}
