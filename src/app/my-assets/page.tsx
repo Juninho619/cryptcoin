aceb4d4b3c869d9223b42b934184b52b2508d257
Christophe <christophebontemps619@gmail.com>
2024-06-14 15:03:39 +0200

    my assets are shown

--- src/app/my-assets/page.tsx
+++ src/app/my-assets/page.tsx
@@ -1,4 +1,4 @@
-import MyAssets from "@/components/user/MyAssets";
+import { MyAssets } from "@/components/user/MyAssets";
 import { myAssets } from "@/service/user";
 import { userAssetsProps } from "@/utils/types";
 import React, { useEffect, useState } from "react";
@@ -6,11 +6,9 @@ import React, { useEffect, useState } from "react";
 const page = () => {
   return (
     <div className='flex flex-wrap items-center'>
-      return (
-      <div className='border-2 border-solid w-80 h-80 rounded-md m-8 p-4 '>
+      <div className='border-2 border-solid w-80 h-80 rounded-md m-8 p-4 w-full'>
         <MyAssets />
       </div>
-      );
     </div>
   );
 };
--- src/components/user/MyAssets.tsx
+++ src/components/user/MyAssets.tsx
@@ -1,61 +1,83 @@
 "use client";
 import { myAssets } from "@/service/user";
-import { userAssetsProps } from "@/utils/types";
+import { cryptoProps, userAssetsProps } from "@/utils/types";
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 
-function MyAssets() {
+export const MyAssets = () => {
   const [assetList, setAssetList] = useState<userAssetsProps[]>();
-
-  const columns: GridColDef[] = [
-    { field: "col1", headerName: "First name", width: 150 },
-    { field: "col2", headerName: "Last name", width: 150 },
-    { field: "col3", headerName: "Money", width: 150 },
-    { field: "col4", headerName: "Pseudo", width: 150 },
-    { field: "col5", headerName: "Age", width: 150 },
-    { field: "col6", headerName: "Id", width: 150 },
-    { field: "col7", headerName: "Name", width: 150 },
-    { field: "col8", headerName: "Value", width: 150 },
-    { field: "col9", headerName: "Image", width: 150 },
-    { field: "col10", headerName: "Quantity", width: 150 },
-    { field: "col11", headerName: "Created", width: 150 },
-    { field: "col12", headerName: "Last update", width: 150 },
-    { field: "col13", headerName: "Amount", width: 150 },
-  ];
-
-  const rows: GridRowsProp = [
-    { firstName: {assetList.firstName},
-    lastName: {assetList.lastName},
-    dollarAvailables: {assetList.dollarAvailables},
-    pseudo: {assetList.pseudo},
-    age: {assetList.age},
-    id: {assetList.id},
-    name: {assetList.name},
-     value: {assetList.value},
-    image: {assetList.image},
-    quantity: {assetList.quantity},
-    created_at: {assetList.created_at},
-    updated_at: {assetList.updated_at},
-    amount: {assetList.amount} }
-  ];
+  const [cryptoList, setCryptoList] = useState<cryptoProps>();
 
   useEffect(() => {
     myAssets()
-      .then((res) => setAssetList(res.data))
+      .then((res) => {
+        console.log(res);
+
+        setAssetList(res.UserHasCrypto);
+      })
       .catch((e) => {
         console.log(e);
       });
   }, []);
 
+  const columns: GridColDef[] = [
+    {
+      field: "A",
+      headerName: "Id",
+      width: 150,
+      renderCell: (params) => {
+        return <p>{params.row.id}</p>;
+      },
+    },
+    {
+      field: "B",
+      headerName: "Name",
+      width: 150,
+      renderCell: (params) => {
+        return <p>{params.row.Crypto.name}</p>;
+      },
+    },
+    {
+      field: "C",
+      headerName: "Money",
+      width: 150,
+      renderCell: (params) => {
+        return <p>{params.row.Crypto.value}</p>;
+      },
+    },
+    {
+      field: "D",
+      headerName: "Pseudo",
+      width: 150,
+      renderCell: (params) => {
+        return <p>{params.row.pseudo}</p>;
+      },
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      width: 150,
+    },
+    {
+      field: "G",
+      headerName: "Image",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <img src={params.row.Crypto.Image} alt={params.row.Crypto.name} />
+        );
+      },
+    },
+  ];
   return (
-    <div>
-      <DataGrid
-        rows={assetList}
-        columns={columns}
-        style={{ minHeight: "100vh", width: "100%" }}
-      />
+    <div className='w-full'>
+      {assetList && assetList.length > 0 && (
+        <DataGrid
+          rows={assetList}
+          columns={columns}
+          style={{ minHeight: "100vh", width: "100%" }}
+        />
+      )}
     </div>
   );
-}
-
-export default MyAssets;
+};
