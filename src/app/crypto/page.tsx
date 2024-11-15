e03515df0ee44245109fd0b1543fb2c4e6da1e41
Juninho619 <christophebontemps619@gmail.com>
2024-11-15 10:23:53 +0100

    removed unused imports

--- src/app/crypto/page.tsx
+++ src/app/crypto/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 import Header from "@/components/Header";
 import { CryptoCard } from "@/components/crypto/CryptoCard";
-import SearchCrypto from "@/components/crypto/SearchCrypto";
-import { BuyCryptoModal } from "@/components/modal/BuyCryptoModal";
-import { getAllCrypto } from "@/service/crypto";
-import { cryptoProps } from "@/utils/types";
 import React, { useEffect, useState } from "react";
 
 function CryptoCards() {
